8e1ebd4b6bfa6e6c0b1707ae275b9d79401cfe7c
Rafał <rgebica@gmail.com>
2020-03-30 16:21:31 +0200

    Components folder push

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { BlogComponent } from './components/blog/blog.component';
+import { ContactComponent } from './components/contact/contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    BlogComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
